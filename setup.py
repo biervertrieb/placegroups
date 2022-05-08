cd61f32d46d995729b1de3b34f3fa897707a0b54
biervertrieb <biervertrieb@googlemail.com>
2022-05-08 11:39:14 +0000

    cookiecutter data science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This software is part of a Big Data Analytics course project made by comp sci students. It aims to find groupings of users in the dataset of r/place with common interests.',
+    author='Derr, Funk, Heuer, Littmann, Wetzel',
+    license='MIT',
+)
