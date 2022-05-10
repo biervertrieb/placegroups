ae4c8070d92835d6ba48dafed29285dde60a83aa
biervertrieb <biervertrieb@googlemail.com>
2022-05-10 15:53:27 +0200

    Fix für arbitrare columns

--- src/features/feature_functions.py
+++ src/features/feature_functions.py
@@ -3,14 +3,11 @@ import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 
 
-def group_dataframe_by_user(df_input: DataFrame) -> DataFrame:
-    ''' takes a dataframe with sourcedata and groups the pixeldata by user '''
-    agg_list = []
-    for columnname in df_input.columns:
-        if columnname == 'user_id':
-            agg_list.append(F.count('*').alias('count'))
-        else:
-            agg_list.append(F.collect_list(columnname))
-    df_output = df_input.groupBy('user_id').agg(
-        agg_list).orderBy(F.count('x').desc())
-    return df_output
+def group_dataframe_by_columns(df_input: DataFrame, columns_to_group: list) -> DataFrame:
+    ''' groups a dataframe by a list of columns. all other columns get aggregatet as a list ov their values '''
+    agg_columns = [
+        colname for colname in df_input.columns if colname not in columns_to_group]
+    exprs = [F.collect_list(colName) for colName in agg_columns]
+    exprs.insert(0, F.count(columns_to_group[0]).alias('count'))
+    df_grouped = df_input.groupby(columns_to_group).agg(*exprs)
+    return df_grouped
